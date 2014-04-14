3493f6fe489c435d6e1a05f6ecf813e59c9c35d9
KatorSi <ShinoGir@gmail.com>
2014-04-14 19:43:50 +0400

    Used Hack language, created method for enumerated templates.

--- web_management/index.php
+++ web_management/index.php
@@ -1,4 +1,4 @@
-<?php
+<?hh
 /**
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,51 +15,15 @@
  *
  * @Author Dmitrii Zolotov <zolotov@gathe.org>, Tikhon Tagunov <tagunov@gathe.org>, Yurii Senin <ShinoGir@gmail.com>
  */
-require_once('config.php');
-setlocale(LC_ALL, LOCALE);
-bindtextdomain("messages", "./locale");
-textdomain("messages");
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title><?php echo _("index title")?></title>
-</head>
-<body>
-<h2><?php echo _("head title")?></h2>
-<?php
-  $t = file(PATH."/documents.properties");
-  $data = array();
-  foreach ($t as $s) {
-    $row = array();
-    $q = explode("=",$s);
-    $id = $q[0];
-    $fn = $q[1];
-    $fn = substr($fn,strrpos($fn,"/")+1);
-    $fn = substr($fn,0,strpos($fn,"."));
-    $row["id"]=$id;
-    $row["prefix"]=$fn;
-    $descfile = file(PATH."/".$fn.".data");
-    $description = substr($descfile[0],1);
-    $row["description"]=$description;
-    $data[]=$row;
+require_once('Render.php');
+require_once('Template.php');
+  $vector = Vector<Template> {};
+  $render = new Render();
+  $render->header("Templates list");
+  $vector = Template::list_template();
+  foreach($vector as $value) {
+      echo $value->getName();
+      echo "<br/>";
   }
-  
-  echo "<table border='1'>";
-  foreach ($data as $row) {
-    echo "<tr>";
-    echo "<td><a href='viewTemplate.php?id=".$row["id"]."'>".$row["id"]."</td>";
-    echo "<td>".$row["description"]."</td>";
-    echo "<td><a href='downloadTemplate.php?id=".$row["id"]."'>[скачать]</a></td>";
-    echo "<td><a href='deleteTemplate.php?id=".$row["id"]."'>[X]</a></td>";
-    echo "<td><a href='replaceTemplate.php?id=".$row["id"]."'>Заменить</a></td>";
-    echo "<td><a href='listSamples.php?id=".$row["id"]."'>Список образцов</a></td>";
-    echo "<td><a href='preview.php?id=".$row["id"]."'>Заполнить образцом</a></td>";
-    echo "</tr>";
-  }
-  echo "</table>";
-?>
-<a href="createTemplate.php">Создать шаблон</a>
-</body>
-</html>
+
+  $render->footer();
